ee37a73787c25526e14e79790953b101d0158a13
Balkı <balki1985@gmail.com>
2018-04-19 22:43:41 +0300

    gmap example

--- data_collector/twitter.js
+++ data_collector/twitter.js
@@ -12,55 +12,6 @@ twitter.authenticate = () => {
     return client;
 };
 
-twitter.search = (filter, allCount, callback) => {
-    let returnList = new Array();
-
-    let client = twitter.authenticate();
-
-    let getTweets = (filter, maxId) => {
-        let options = {
-            q: filter,
-            result_type: 'recent',
-            //lang: 'tr',
-            count: 100,
-            include_entities:true
-        };
-
-        if(maxId){
-            options.max_id = maxId;
-        }
-
-        client.get('search/tweets', options, (error, tweets, response) => {
-            if (error) {
-                console.error(error.message || error[0].message);
-                return callback(returnList);
-            }
-
-            let statuses = tweets.statuses;
-            if (statuses && statuses.length > 0) {
-                for (let status of statuses) {
-                    if(status.geo){
-                        returnList.push(status);
-                        console.info(status.text);
-                    }
-                }
-
-                if (returnList.length < allCount) {
-                    getTweets(filter, statuses[statuses.length-1].id);
-                }
-                else {
-                    return callback(returnList);
-                }
-            }
-            else {
-                return callback(returnList);
-            }
-        });
-    };
-
-    getTweets(filter);
-};
-
 twitter.stream = (callback) => {
     let client = twitter.authenticate();
 
@@ -68,7 +19,7 @@ twitter.stream = (callback) => {
         let stream = client.stream('statuses/filter', {track: '.'});
         stream.on('data', (tweet) => {
             if(tweet && tweet.geo){
-                console.info(tweet.text + '\t' + tweet.geo);
+                console.info(tweet.text + '\t' + JSON.stringify(tweet.geo));
                 callback(tweet);
             }
         });
